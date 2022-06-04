2ce1080afe4254e5bf874fa450669c2d2961b68d
Justin <justin@justins-imac.lan>
2022-06-03 21:35:17 -0700

    added public folder

--- app.js
+++ app.js
@@ -2,8 +2,18 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.json())
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send(`
+  <h2>Reciprocity Image API</h2>
+  
+  <p>Example endpoint:</p>
+  
+  <a href="compressed-1495195134817-aeb325a55b65-Lukas-Blazek.jpg">compressed-1495195134817-aeb325a55b65-Lukas-Blazek.jpg</a>
+
+  `)
 })
 
 app.listen(port, () => {
